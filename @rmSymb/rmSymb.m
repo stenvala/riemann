b38136b140b61c86b105b0d40e8af187d5eb7630
Antti Stenvall <antti@stenvall.fi>
2014-05-23 14:17:54 +0300

    Set private

--- @rmSymb/rmSymb.m
+++ @rmSymb/rmSymb.m
@@ -1,17 +1,33 @@
 classdef rmSymb < handle
   % Class for perfoming symbolic operation on Riemannian manifold
   %
+  % properties:
+  %   - metricTensor: metric tensor of the manifold (function handle to
+  %     symbolic function)
+  %  
   % varargin:  
   %   - dim {2}: dimension of the manifold
+  %   - title {''}: title of the manifold
+  %   - description{''}: description of the manifold
   %
   % Created: Antti Stenvall (antti@stenvall.fi)
   %
   properties (GetAccess=protected,SetAccess=protected)
-    metricTensor
+    g
+    dim
+    title
+    description
   end
   methods (Access=public)
-    function this = rmSymb(varargin)
-      
+    function this = rmSymb(metricTensor,varargin)
+        defaults.dim = 2;
+        defaults.description = '';
+        defaults.title = '';
+        params = setDefaultParameters(defaults,varargin);
+        this.g = metricTensor;
+        this.dim = params.dim;
+        this.description = params.description;
+        this.title = params.title;
     end
   end
   methods (Access=protected)
